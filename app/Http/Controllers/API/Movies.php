a20444bd2abe0add5364159ad6c8e57c994891e2
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-08 18:24:13 +0100

    index, store, update and delete methods created and working in movies controller

--- app/Http/Controllers/API/Movies.php
+++ app/Http/Controllers/API/Movies.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Request\API\MovieRequest;
+use App\Http\Resources\API\MovieResource;
+use App\Movie;
 
 class Movies extends Controller
 {
+
+    protected $movie;
+
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+        $this->movie = new Movie;
+    }
     /**
-     * Display a listing of the resource.
+     * Display a listing of the movies.
      *
      * @return \Illuminate\Http\Response
      */
@@ -18,18 +29,32 @@ class Movies extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created movie in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $user_token = $request->token;
+        $user = auth("users")->authenticate($user_token);
+        $user_id = $user->id;
+
+        $this->movie->user_id = $user_id;
+        $this->movie->movieTitle = $request->movieTitle;
+        $this->movie->movieDirector = $request->movieDirector;
+        $this->movie->movieGenre = $request->movieGenre;
+        $this->movie->movieCast = $request->movieCast;
+        $this->movie->save();
+
+        return response()->json([
+            "success" => true,
+            "message" => "movie saved successfully"
+        ], 200);
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified movie.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -40,15 +65,22 @@ class Movies extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified movie in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $movie)
     {
-        //
+        // get the request data
+        $data = $request->all();
+
+        // update the player
+        $movie->fill($data)->save();
+
+        // return the updated movie in the resource format
+        return new MovieResource($movie);
     }
 
     /**
@@ -57,8 +89,11 @@ class Movies extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Movie $movie)
     {
-        //
+        $movie->delete();
+
+        // use a 204 code as there is no content in the response
+        return response(null, 204);
     }
 }

de1911ae1c2a08df010a5a3b6416b2fe99677af8
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-08 14:35:42 +0100

    movie api routes created dealing with crud operations for user's movies

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Users\AuthController;
+use App\Http\Controllers\API\Movies;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,5 +24,24 @@ Route::group(
     function () {
         Route::post('register', [AuthController::class, "register"]);
         Route::post('login', [AuthController::class, "login"]);
+        Route::post("movie/add", "Movies@store");
+        Route::get("movie/get-all/{token}", "Movies@index");
+        Route::patch("movie/update/{id}", "Movies@update");
+        Route::delete("movie/delete/{id}", "Movies@destroy");
+        Route::get("movie/get-single/{id}", "Movies@getSingleData");
+    }
+);
+
+Route::group(
+    [
+        'prefix' => 'user',
+        'namespace' => 'API',
+    ],
+    function () {
+        Route::post("movies", "Movies@store");
+        Route::get("movies", "Movies@index");
+        Route::patch("movie/{movie}", "Movies@update");
+        Route::delete("movie/{movie}", "Movies@destroy");
+        Route::get("movie/{movie}", "Movies@getSingleData");
     }
 );

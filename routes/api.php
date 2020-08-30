b7cbf53cc5d533a24d6fd188652c36d83f110fdc
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-30 15:37:10 +0100

    new route strcuture layed out in preparation for introduction of policies

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Policies\UserAccessMoviePolicy;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Auth\Middleware\Authorize;
 use App\Http\Controllers\API\User\AuthController;
 use App\Http\Controllers\API\User\Movies;
 
@@ -15,6 +17,26 @@ use App\Http\Controllers\API\User\Movies;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// authorization via policies in progress
+
+// Route::group(['prefix' => 'user'], function () {
+//     Route::post('register', [AuthController::class, "register"]);
+//     Route::post('login', [AuthController::class, "login"]);
+
+//     Route::group(['prefix' => '{user}'], function () {
+
+//         Route::group(['prefix' => 'movies', 'middleware' => 'can:UserAccess,movie'], function () {
+//             Route::get("", [Movies::class, "index"]);
+//             Route::post("", [Movies::class, "store"]);
+
+//             Route::group(['prefix' => '{movie}', 'middleware' => ['can:UserAccess,movie']], function () {
+//                 Route::put("{movie}", [Movies::class, "update"]);
+//                 Route::delete("{movie}", [Movies::class, "destroy"]);
+//             });
+//         });
+//     });
+// });
 
 Route::group(['prefix' => 'user'], function () {
     Route::post('register', [AuthController::class, "register"]);
